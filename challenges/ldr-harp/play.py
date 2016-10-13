361e03386779b5a3eceac435f24162c5b5fc720f
Kuanysh Kerimov <kuanysh.kerimov@gmail.com>
2016-10-13 16:54:51 +0800

    Changed the code to use the COM PORT specified inside

--- challenges/ldr-harp/play.py
+++ challenges/ldr-harp/play.py
@@ -5,34 +5,35 @@ import serial
 
 args = sys.argv
 print args
-if len(args) < 2:
-	print "Please specify COM port"
-	sys.exit(1)
-strPort = args[1]
+#if len(args) < 2:
+    #print "Please specify COM port"
+    #sys.exit(1)
+
+#CHANGE THE COM PORT ACCORDING TO YOUR ARDUINO APP
+strPort = "COM8" #args[1]
 
 soundfile = "loop_amen_full.wav"
 
 if len(args) > 2:
-	soundfile = args[2]
-	
+    soundfile = args[2]
 
 mixer.pre_init(44100, -16, 12, 512)
 mixer.init()
 
-sound =  mixer.Sound(soundfile)
+sound = mixer.Sound(soundfile)
 
-#strPort = '/dev/ttyACM0'
-#strPort = 'COM19'
+# strPort = '/dev/ttyACM0'
+# strPort = 'COM19'
 ser = serial.Serial(strPort, 9600)
 
-#sound.play()
-#time.sleep(3)
+sound.play()
+# time.sleep(3)
 while 1:
     line = ser.readline().strip()
-    print line 
-    if line == 'ON': 
-        sound.play(loops = -1)
+    print line
+    if line == 'ON':
+        sound.play(loops=-1)
     if line == 'OFF':
         sound.stop()
-		
+
 
