ec64e49637f4bdaf50bd7635e005f1d678064f90
Rohan Ahmad <rohanoahmad@gmail.com>
2016-10-13 11:13:59 +0800

    Added challenges

---
+++ challenges/ldr-harp/ldr_harp.ino
@@ -0,0 +1,14 @@
+
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  
+
+  
+}
---
+++ challenges/temp-logger/temp-logger.ino
@@ -0,0 +1,18 @@
+#include "cactus_io_DS18B20.h" 
+
+// Create DS18B20 object 
+DS18B20 ds(DS18B20_Pin); 
+
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  
+
+  
+}
