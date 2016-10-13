4d4887a1470ea4c5e567023ed6c9acbebf5ade70
Kuanysh Kerimov <kuanysh.kerimov@gmail.com>
2016-10-13 11:37:03 +0800

    Slightly increased delay

--- challenges/ldr-harp/ldr_harp.ino
+++ challenges/ldr-harp/ldr_harp.ino
@@ -8,9 +8,9 @@ void setup() {
 }
 
 void loop() {
-  // 5V/1024 = 0.0048828125
+  // 5V/1024 = 0.0048828125   
   float volts = analogRead(IRpin)*0.0048828125;   
   float distance = 65*pow(volts, -1.10);         
   Serial.println(distance);                      
-  delay(100);                                     
+  delay(200);                                     
 }
