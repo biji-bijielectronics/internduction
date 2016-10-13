ce24327c69d6297a91a2610a28427e3b4bd900dd
Kuanysh Kerimov <kuanysh.kerimov@gmail.com>
2016-10-13 11:31:59 +0800

    Added IR Sharp code

--- challenges/ldr-harp/ldr_harp.ino
+++ challenges/ldr-harp/ldr_harp.ino
@@ -1,14 +1,16 @@
 
 
 // the setup routine runs once when you press reset:
+int IRpin = A0;                                    
+
 void setup() {
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
-  
+  Serial.begin(9600);                            
 }
 
 void loop() {
-  
-
-  
+  // 5V/1024 = 0.0048828125
+  float volts = analogRead(IRpin)*0.0048828125;   
+  float distance = 65*pow(volts, -1.10);         
+  Serial.println(distance);                      
+  delay(100);                                     
 }
